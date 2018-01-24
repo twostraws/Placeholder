ec4376216fc2743c1047863db298fc6a5163f626
Paul Hudson <paul.hudson@gmail.com>
2018-01-24 20:53:29 +0000

    Added Placeholder.swift.

---
+++ Placeholder.swift
@@ -0,0 +1,124 @@
+//
+// MIT License
+//
+// Copyright (c) 2018 Paul Hudson
+// https://www.github.com/twostraws/Placeholder
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+import UIKit
+
+/**
+Placeholder is a `UIImageView` subclass that makes it easy to show
+ placeholder image data in layouts. By default it renders the size of
+ the placeholder, but you can also provide a title.
+ */
+@IBDesignable class Placeholder: UIImageView {
+    /// Title text to render inside the image
+    @IBInspectable var title: String = ""
+
+    /// The color of the title text
+    @IBInspectable var textColor: UIColor = .white
+
+    /// Whether to round the edges of the placeholder graphic.
+    /// This automatically rounds to the smallest of the width
+    /// or height of the view.
+    @IBInspectable var rounded: Bool = false
+
+    /// The label used to render title and size text
+    private var label: UILabel!
+
+    override required init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configure()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configure()
+    }
+
+    /** This configures the size label according to the current
+     settings. This will get called whenever the image view
+     is re-laid out (e.g. after rotation).
+    */
+    private func configure() {
+        if label == nil {
+            // make sure we don't render the label outside of ourselves
+            clipsToBounds = true
+
+            label = UILabel()
+            label.numberOfLines = 0
+            label.textAlignment = .center
+            addSubview(label)
+
+            // center the label inside the placeholder space
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+            label.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        }
+
+        // apply rounding as needed
+        if rounded == true {
+            layer.cornerRadius = min(bounds.midX, bounds.midY)
+        }
+
+        // put the width and height for this view into a string
+        let width = Int(bounds.width)
+        let height = Int(bounds.height)
+        let size = "\(width)x\(height)"
+
+        // configure attributes of the size text
+        let sizeAttrs =  [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 20)]
+        let sizeString = NSAttributedString(string: size, attributes: sizeAttrs)
+
+        if title.isEmpty {
+            // if we don't have a size render the text immediately
+            label.attributedText = sizeString
+        } else {
+            // if we do have a title, prepare that with some attributes that are slightly
+            // bigger than the size
+            let titleAttrs = [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 24)]
+            let titleString = NSMutableAttributedString(string: "\(title)\n", attributes: titleAttrs)
+
+            // append the size string and put it inside the label
+            titleString.append(sizeString)
+            label.attributedText = titleString
+        }
+
+        // make sure the label color reflects the latest setting
+        label.textColor = textColor
+    }
+
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        configure()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        configure()
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Placeholder
-Place temporary images in your iOS app showing the size of the available space.
+
+When you’re busy prototyping an app, it’s helpful to be able to see at a glance what images still need to be filled and what size they are. Placeholder does exactly that: it’s a `UIImageView` subclass that automatically renders its size along with any title you want.
+
+![Placeholder overlays an image’s size and title so you can see exactly what’s missing.](https://www.hackingwithswift.com/files/placeholder/screenshot.png)
+
+To install, just drag Placeholder.swift into your Xcode project, then make change the class of any placeholder images to be `Placeholder` rather than `UIImageView`. When the final artwork arrives, all you need to do is change the class back to `UIImageView` and you’re back to normal – easy!
+
+**Note:** Xcode 9.2 and earlier overlay the words “UIImageView” on all `UIImageView` subclasses, but this only occurs at design time. This bug is fixed in Xcode 9.3.
+
+
+## Options
+
+There are three properties for `Placeholder` images that you might want to customize, all of which are exposed to Interface Builder:
+
+- `title` lets you add an optional string to be placed above the size, e.g. “User avatar”.
+- `textColor` lets you adjust the text color used to render the title and size. Defaults to `.white`.
+- `rounded` rounds the corners of the image view using the minimum of the width and height. This creates lozenges in rectangular views and circles in square views.
+
+
+## License
+
+MIT License.
+
+Copyright (c) 2018 Paul Hudson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
